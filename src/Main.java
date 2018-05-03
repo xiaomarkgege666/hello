b81d1ad6df80f2602fd8cf2896acec62831222d9
mama <584935163@qq.com>
2018-05-03 23:54:31 +0800

    test11111

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Main {
+    private FileOutputStream fff;
+    public static void main(String[] args) {
+
+        String s = "ss";
+        if(s!=null) {
+            System.out.println("new:" );
+            String s2    = "ssss";
+        }
+        ArrayList list = new ArrayList();
+
+        for(int i=0;i<list.size();i++){
+
+        }
+
+
+       /* String s = "ssss";
+        String s = "ssss";*/
+    }
+}
